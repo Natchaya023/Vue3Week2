8c9b540a5c300ae75e525520f3438f7924f50498
Natchaya023 <sakoleek93@gmail.com>
2022-10-27 16:32:34 +0700

    UP

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "Hello World!",
+            fname: "Mark Zuckerberg",
+            count: 0,
+        }
+    }
+}).mount("#app")
